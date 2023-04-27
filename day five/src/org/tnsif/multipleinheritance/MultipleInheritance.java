82f3ab6ed13392314bd72e90c0aa99e7a609d3bf
SHINDE <SHINDE@DESKTOP-KKG7GLD>
2023-04-27 13:43:01 +0530

    Day five resolve it again

---
+++ day five/src/org/tnsif/multipleinheritance/MultipleInheritance.java
@@ -0,0 +1,17 @@
+package org.tnsif.multipleinheritance;
+
+public class MultipleInheritance {
+
+	public static void main(String[] args) {
+		City c=new City();
+		c.setCityName("Pune");
+		c.setArea("Keshav Nagar");
+		c.setStateName("Maharashtra");
+		c.setCountryName("India");
+		c.setCountryCapital("Delhi");
+		System.out.println(c);
+	}
+
+	}
+
+
