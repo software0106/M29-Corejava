a7cb1115e1ccb267b018b681b43761f91ed8287f
SHINDE <SHINDE@DESKTOP-KKG7GLD>
2023-06-15 15:14:07 +0530

    day six

---
+++ daysix/src/org/tnsif/finalkeyword/FinalExecutor.java
@@ -0,0 +1,17 @@
+package org.tnsif.finalkeyword;
+
+public class FinalExecutor {
+	public static void main(String[] args) {
+		FinalVariable f=new FinalVariable();
+		f.display();
+		
+		FinalMethodChildClass f1=new FinalMethodChildClass();
+		f1.show(500);
+		// TODO Auto-generated method stub
+		FinalClass f2=new FinalClass();
+		f2.show();
+	}
+
+}
+
+
---
+++ daysix/src/org/tnsif/finalkeyword/FinalMethod.java
@@ -0,0 +1,16 @@
+package org.tnsif.finalkeyword;
+
+public class FinalMethod {
+
+	public FinalMethod() {
+		System.out.println("Default constructor");
+		// TODO Auto-generated constructor stub
+	}
+	final float salary=27000.85f;
+	final void show() {
+		System.out.println("Salary is "+salary);
+		
+	}
+	
+	
+}
